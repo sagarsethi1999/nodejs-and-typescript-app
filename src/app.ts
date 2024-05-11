840d5322aadcff03f638310bb47a141825e2e7fc
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-05-11 10:48:02 +0530

    second commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+// import express = require('express');
+
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
+import todosRoutes from './routes/todos';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosRoutes);
+
+ app.listen({port: 3000});
---
+++ src/routes/todos.ts
@@ -0,0 +1,48 @@
+import {Router} from 'express';
+
+import { Todo } from '../models/todo';
+import { todo } from 'node:test';
+
+let todos: Todo[] = [];
+
+const router = Router();
+
+type RequestBody = { text : string };
+type RequestParams = { todoId : string };
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({todos: todos});
+})
+
+router.get('/todo',(req, res, next) => {
+    const body =  req.body as RequestBody;
+    const newTodo: Todo = {
+        id: new Date().toString(),
+        text:body.text
+    };
+
+    todos.push(newTodo);
+    res.status(201).json({message: 'Added todo', todo: newTodo, todos: todos});
+})
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+    if(todoIndex >=0 ){
+        todos[todoIndex] = {id: todos[todoIndex].id, text: body.text};
+        return res.status(200).json({message: 'Updated todo', todos: todos});
+    }
+    res.status(404).json({ message: 'Could not find todo for thid id.'});
+});
+
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    const params = req.params as RequestParams;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+    res.status(200).json({message: 'Deleted todo', todos: todos});
+});
+
+
+export default router;
